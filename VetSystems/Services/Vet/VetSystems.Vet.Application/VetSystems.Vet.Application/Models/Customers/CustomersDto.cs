6e872dcc3843a01bd3aa840b733391e92725b1a1
Dogan Gunes <dogangns.98@gmail.com>
2023-07-15 02:32:17 +0300

    .

--- VetSystems/Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Models/Customers/CustomersDto.cs
+++ VetSystems/Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Models/Customers/CustomersDto.cs
@@ -10,11 +10,11 @@ namespace VetSystems.Vet.Application.Models.Customers
     {
         public string FisrName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public int PhoneNumber { get; set; }
-        public int PhoneNumber2 { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PhoneNumber2 { get; set; }
         public string EMail { get; set; } = string.Empty;
         public string TaxOffice { get; set; } = string.Empty;
-        public int VKNTCNo { get; set; }
+        public string VKNTCNo { get; set; }
         public Guid CustomerGroup { get; set; }
         public string Note { get; set; } = string.Empty;
         public decimal DiscountRate { get; set; } = 0;

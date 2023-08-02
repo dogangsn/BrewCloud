fb159b0a407e81d65ca570fb150bf687f61eee7e
Dogan Gunes <dogangns.98@gmail.com>
2023-08-03 01:01:53 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/Definition/ProductDescription/Commands/CreateProductDescriptionCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetSystems.Vet.Application.Features.Definition.ProductDescription.Commands
+{
+    public class CreateProductDescriptionCommand
+    {
+    }
+}

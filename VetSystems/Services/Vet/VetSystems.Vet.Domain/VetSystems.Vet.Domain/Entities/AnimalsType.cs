88305a9b61f7979ce1b294970408f328b2783790
Dogan Gunes <dogangns.98@gmail.com>
2023-08-20 23:17:22 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Domain/VetSystems.Vet.Domain/Entities/AnimalsType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetSystems.Vet.Domain.Common;
+
+namespace VetSystems.Vet.Domain.Entities
+{
+    public class AnimalsType : BaseEntity, IAggregateRoot
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty; 
+    }
+}

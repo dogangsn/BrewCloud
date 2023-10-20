d48e2424ce414c4162ca2ae533ef7a942cd0fc19
Dogan Gunes <dogangns.98@gmail.com>
2023-10-20 12:01:02 +0300

    .

--- VetSystems/Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Migrations/20231020085949_updatedata_4.Designer.cs
+++ VetSystems/Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Migrations/20231020085949_updatedata_4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VetSystems.Vet.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using VetSystems.Vet.Infrastructure.Persistence;
 namespace VetSystems.Vet.Infrastructure.Migrations
 {
     [DbContext(typeof(VetDbContext))]
-    partial class VetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231020085949_updatedata_4")]
+    partial class updatedata_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VetSystems/Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Migrations/20231020085949_updatedata_4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VetSystems.Vet.Infrastructure.Migrations
+{
+    public partial class updatedata_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vetdemandproducts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    id1 = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    remark = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    quantity = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    unitprice = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    stockstate = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    isactive = table.Column<int>(type: "int", nullable: true),
+                    reserved = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    recid = table.Column<int>(type: "int", nullable: false),
+                    createdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleteddate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deletedusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    updateusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createusers = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("VetDemandProducts_pkey", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vetdemandproducts");
+        }
+    }
+}

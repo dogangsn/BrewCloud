7b9728374ecd2a388d63b0562d2dbaf751f10aa7
Dogan Gunes <dogangns.98@gmail.com>
2023-11-15 00:10:21 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/EntityConfigurations/FarmsConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetSystems.Vet.Domain.Entities;
+
+namespace VetSystems.Vet.Infrastructure.EntityConfigurations
+{
+    public class FarmsConfig : IEntityTypeConfiguration<VetFarms>
+    {
+        public void Configure(EntityTypeBuilder<VetFarms> entity)
+        {
+            entity.HasKey(e => e.Id)
+                   .HasName("VetFarms_pkey");
+
+            entity.Property(x => x.RecId)
+           .ValueGeneratedOnAdd()
+           .HasColumnName("recid")
+           .UseIdentityColumn(1, 1);
+        }
+    }
+}
---
+++ VetSystems/Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Migrations/20231114210937_updateData_9.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VetSystems.Vet.Infrastructure.Migrations
+{
+    public partial class updateData_9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vetfarms",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    customerid = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    farmname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    farmcontact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    farmrelationship = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    active = table.Column<bool>(type: "bit", nullable: true),
+                    recid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    createdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleteddate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deletedusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    updateusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createusers = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("VetFarms_pkey", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vetfarms");
+        }
+    }
+}

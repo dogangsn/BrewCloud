7841515fcc19b7a4ad8b37ba92ba663d41ecdff4
Dogan Gunes <dogangns.98@gmail.com>
2023-11-15 18:00:13 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Migrations/20231115145927_updateData_11.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VetSystems.Vet.Infrastructure.Migrations
+{
+    public partial class updateData_11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vetappointments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    begindate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    enddate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    note = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    doctorid = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    customerid = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleteddate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deletedusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    updateusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createusers = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("VetAppointments_pkey", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vetappointments");
+        }
+    }
+}

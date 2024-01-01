0dc3e3f7ca7dbc5e9fd846a0b40b53cf2bf5c8b2
Dogan Gunes <dogangns.98@gmail.com>
2024-01-02 01:46:42 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Models/Dashboards/DashboardsDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetSystems.Vet.Application.Models.Dashboards
+{
+    public class DashboardsDto
+    {
+        public DashboardsDto()
+        {
+            TotalCount = new DashboardCountTotal();
+        }
+        public DashboardCountTotal? TotalCount { get; set; }
+
+    }
+
+    public class DashboardCountTotal
+    {
+        public int DailyAddAppointmentCount { get; set; } = 0;
+        public int DailyAddAppointmentCompletedCount { get; set; } = 0;
+        public int DailyAddCustomerCount { get; set; } = 0;
+        public int DailyAddCustomerYestardayCount { get; set; }
+        public decimal? DailyTurnoverAmount { get; set; } = 0;
+        public decimal? DailyTurnoverPreviousAmount { get; set; } = 0;
+        public decimal? TotalStockAmount { get; set; } = 0;
+    }
+
+}

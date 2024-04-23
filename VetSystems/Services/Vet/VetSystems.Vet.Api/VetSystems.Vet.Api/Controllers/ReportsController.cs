b2f177e18da8ef96dc415478b3e1eb8711479976
Deniz Can TOŞUR <denizcantosur@hotmail.com>
2024-04-23 16:37:59 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Api/VetSystems.Vet.Api/Controllers/ReportsController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VetSystems.Vet.Application.Features.Clinicalstatistics.Queries;
+using VetSystems.Vet.Application.Features.Reports.Commands;
+
+namespace VetSystems.Vet.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ReportsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost(Name = "CreateReportFilter")]
+        public async Task<IActionResult> CreateReportFilter([FromBody] CreateReportFilterCommand model)
+        {
+            return Ok(await _mediator.Send(model));
+        }
+    }
+}
+    // [Route("api/[controller]/[action]")]
+    //[ApiController]
+    //public class ClinicalstatisticsController : ControllerBase
+    //{
+    //    private readonly IMediator _mediator;
+    //    public ClinicalstatisticsController(IMediator mediator)
+    //    {
+    //        _mediator = mediator;
+    //    }
+
+    //}

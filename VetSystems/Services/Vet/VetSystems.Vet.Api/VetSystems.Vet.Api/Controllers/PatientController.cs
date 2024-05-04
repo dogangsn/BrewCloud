c5145dfa598507e2394669a4d52266c84e92793b
Dogan Gunes <dogangns.98@gmail.com>
2024-05-04 14:25:41 +0300

    .

---
+++ VetSystems/Services/Vet/VetSystems.Vet.Api/VetSystems.Vet.Api/Controllers/PatientController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VetSystems.Vet.Application.Features.GeneralSettings.Users.Queries;
+using VetSystems.Vet.Application.Features.Patient.PatientList.Queries;
+
+namespace VetSystems.Vet.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PatientController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #region PatientList
+
+        [HttpGet(Name = "GetPatientList")]
+        public async Task<IActionResult> GetPatientList()
+        {
+            var command = new GetPatientListQuery();
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ VetSystems/Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Models/Patients/PatientList/PatientOwnerListDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetSystems.Vet.Application.Models.Patients.PatientList
+{
+    public class PatientOwnerListDto
+    {
+        public string CustomerFirsLastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AnimalTypeName { get; set; }
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string ChipNumber { get; set; }
+        public int Sex { get; set; }
+        public string AnimalType { get; set; }
+        public string BreedType { get; set; }
+        public string AnimalColor { get; set; }
+        public string ReportNumber { get; set; }
+        public string SpecialNote { get; set; }
+        public bool Sterilization { get; set; }
+        public bool Active { get; set; }
+        public string Thumbnail { get; set; } = string.Empty;
+        public byte[] Images { get; set; }
+    }
+}

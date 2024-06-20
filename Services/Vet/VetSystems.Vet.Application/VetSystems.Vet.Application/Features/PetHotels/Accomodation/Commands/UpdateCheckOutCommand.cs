518a7cc0239483acf895286c59a34d4daf97a01d
Dogan Gunes <dogangns.98@gmail.com>
2024-06-21 01:03:13 +0300

    .

---
+++ Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/PetHotels/Accomodation/Commands/UpdateCheckOutCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetSystems.Shared.Dtos;
+
+namespace VetSystems.Vet.Application.Features.PetHotels.Accomodation.Commands
+{
+    public class UpdateCheckOutCommand : IRequest<Response<bool>>
+    {
+    }
+
+    public class UpdateCheckOutCommandHandler : IRequestHandler<UpdateCheckOutCommand, Response<bool>>
+    {
+
+
+        public async Task<Response<bool>> Handle(UpdateCheckOutCommand request, CancellationToken cancellationToken)
+        {
+
+            var response = Response<bool>.Success(200);
+            try
+            {
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return Response<bool>.Fail(ex.Message, 400);
+            }
+            return response;
+
+        }
+    }
+}

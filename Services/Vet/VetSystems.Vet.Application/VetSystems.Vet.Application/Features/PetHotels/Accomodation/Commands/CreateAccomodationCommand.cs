5b9d7be745b15e35ba5beec1aed6417dd589e9c6
Dogan Gunes <dogangns.98@gmail.com>
2024-06-24 19:03:21 +0300

    .

--- Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/PetHotels/Accomodation/Commands/CreateAccomodationCommand.cs
+++ Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/PetHotels/Accomodation/Commands/CreateAccomodationCommand.cs
@@ -33,9 +33,10 @@ namespace VetSystems.Vet.Application.Features.PetHotels.Accomodation.Commands
         private readonly IMapper _mapper;
         private readonly ILogger<CreateAccomodationCommandHandler> _logger;
         private readonly IRepository<VetAccomodation> _vetAccomodationRepository;
+        private readonly IRepository<VetRooms> _vetRoomsRepository;
         private readonly IMediator _mediator;
 
-        public CreateAccomodationCommandHandler(IUnitOfWork uow, IIdentityRepository identity, IMapper mapper, ILogger<CreateAccomodationCommandHandler> logger, IRepository<VetAccomodation> vetAccomodationRepository, IMediator mediator)
+        public CreateAccomodationCommandHandler(IUnitOfWork uow, IIdentityRepository identity, IMapper mapper, ILogger<CreateAccomodationCommandHandler> logger, IRepository<VetAccomodation> vetAccomodationRepository, IMediator mediator, IRepository<VetRooms> vetRoomsRepository)
         {
             _uow = uow;
             _identity = identity;
@@ -43,6 +44,7 @@ namespace VetSystems.Vet.Application.Features.PetHotels.Accomodation.Commands
             _logger = logger;
             _vetAccomodationRepository = vetAccomodationRepository;
             _mediator = mediator;
+            _vetRoomsRepository = vetRoomsRepository;
         }
 
         public async Task<Response<bool>> Handle(CreateAccomodationCommand request, CancellationToken cancellationToken)
@@ -58,20 +60,36 @@ namespace VetSystems.Vet.Application.Features.PetHotels.Accomodation.Commands
             TimeZoneInfo localTimeZone = TimeZoneInfo.Local;
             try
             {
+
+                DateTime _checkinDate = TimeZoneInfo.ConvertTime(Convert.ToDateTime(request.CheckinDate), localTimeZone);
+                DateTime _checkotDate = TimeZoneInfo.ConvertTime(Convert.ToDateTime(request.CheckOutDate), localTimeZone);
+
+
+                var _controlRoom = (await _vetAccomodationRepository.GetAsync(x => x.Deleted == false
+                && x.RoomId == request.RoomId
+                && ((x.CheckinDate <= _checkinDate && x.CheckOutDate >= _checkinDate)
+                    || (x.CheckinDate <= _checkotDate && x.CheckOutDate >= _checkotDate)
+                    || (x.CheckinDate >= _checkinDate && x.CheckOutDate <= _checkotDate)))).FirstOrDefault();
+
+                if (_controlRoom != null)
+                {
+                    var roomsInfo = await _vetRoomsRepository.GetByIdAsync(request.RoomId);
+                    return Response<bool>.Fail(roomsInfo.RoomName + " Konaklama Mevcuttur. Lütfen Kontrol Ediniz.", 404);
+                }
+
                 VetAccomodation accomodation = new()
                 {
                     Type = request.Type,
                     RoomId = request.RoomId,
                     CustomerId = request.CustomerId,
                     PatientsId = request.PatientsId,
-                    CheckinDate = TimeZoneInfo.ConvertTime(Convert.ToDateTime(request.CheckinDate), localTimeZone),
-                    CheckOutDate = TimeZoneInfo.ConvertTime(Convert.ToDateTime(request.CheckOutDate), localTimeZone),
+                    CheckinDate = _checkinDate,
+                    CheckOutDate = _checkotDate,
                     Accomodation = (AccomodationType)request.Accomodation,
                     Remark = request.Remark,
                     CreateDate = DateTime.Now,
                     CreateUsers = _identity.Account.UserName
                 };
-
                 await _vetAccomodationRepository.AddAsync(accomodation);
 
                 await _uow.SaveChangesAsync(cancellationToken);
--- Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/PetHotels/Accomodation/Commands/UpdateCheckOutCommand.cs
+++ Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/PetHotels/Accomodation/Commands/UpdateCheckOutCommand.cs
@@ -5,16 +5,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VetSystems.Shared.Dtos;
+using VetSystems.Vet.Domain.Entities;
+using VetSystems.Vet.Domain.Contracts;
+using VetSystems.Shared.Service;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
 
 namespace VetSystems.Vet.Application.Features.PetHotels.Accomodation.Commands
 {
     public class UpdateCheckOutCommand : IRequest<Response<bool>>
     {
+        public Guid? CustomerId { get; set; }
+        public Guid? PatientsId { get; set; }
+        public Guid RoomId { get; set; }
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public decimal AccomodationAmount { get; set; }
+        public decimal CollectionAmount { get; set; }
+        public Guid PaymentId { get; set; }
     }
 
     public class UpdateCheckOutCommandHandler : IRequestHandler<UpdateCheckOutCommand, Response<bool>>
     {
+        private readonly IUnitOfWork _uow;
+        private readonly IIdentityRepository _identity;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateCheckOutCommandHandler> _logger;
+        private readonly IRepository<VetAccomodation> _vetAccomodationRepository;
+        private readonly IMediator _mediator;
 
+        public UpdateCheckOutCommandHandler(IUnitOfWork uow, IIdentityRepository identity, IMapper mapper, ILogger<UpdateCheckOutCommandHandler> logger, IRepository<VetAccomodation> vetAccomodationRepository, IMediator mediator)
+        {
+            _uow = uow;
+            _identity = identity;
+            _mapper = mapper;
+            _logger = logger;
+            _vetAccomodationRepository = vetAccomodationRepository;
+            _mediator = mediator;
+        }
 
         public async Task<Response<bool>> Handle(UpdateCheckOutCommand request, CancellationToken cancellationToken)
         {
@@ -22,6 +50,9 @@ namespace VetSystems.Vet.Application.Features.PetHotels.Accomodation.Commands
             var response = Response<bool>.Success(200);
             try
             {
+
+
+
 
 
 

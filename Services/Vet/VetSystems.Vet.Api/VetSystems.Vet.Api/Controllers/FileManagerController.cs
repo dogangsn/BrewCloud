bfe367ecb16c768ef13d6c8dd4208993b0386ecb
Dogan Gunes <dogangns.98@gmail.com>
2024-06-28 21:23:12 +0300

    .

---
+++ Services/Vet/VetSystems.Vet.Api/VetSystems.Vet.Api/Controllers/FileManagerController.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VetSystems.Vet.Application.Features.Definition.Taxis.Commands;
+using VetSystems.Vet.Application.Features.Definition.Taxis.Queries;
+using VetSystems.Vet.Application.Features.FileManager.Commands;
+using VetSystems.Vet.Application.Features.FileManager.Queries;
+
+namespace VetSystems.Vet.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FileManagerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public FileManagerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #region FileManager
+
+        [HttpGet(Name = "GetFileManagerList")]
+        public async Task<IActionResult> GetFileManagerList()
+        {
+            var command = new GetFileManagerListQuery();
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPost(Name = "CreateFileManager")]
+        public async Task<IActionResult> CreateFileManager([FromForm] CreateFileManagerCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+         
+        [HttpPost(Name = "DeleteFileManager")]
+        public async Task<IActionResult> DeleteFileManager([FromBody] DeleteFileManagerCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+         
+        [HttpPost(Name = "GetFileManagerForById")]
+        public async Task<IActionResult> GetFileManagerForById([FromBody] GetFileManagerForByIdQuery command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Services/Vet/VetSystems.Vet.Application/VetSystems.Vet.Application/Features/FileManager/Queries/GetFileManagerForByIdQuery.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetSystems.Shared.Dtos;
+using VetSystems.Shared.Service;
+using VetSystems.Vet.Application.Models.FileManager;
+using VetSystems.Vet.Domain.Contracts;
+using VetSystems.Vet.Domain.Entities;
+
+namespace VetSystems.Vet.Application.Features.FileManager.Queries
+{
+    public class GetFileManagerForByIdQuery : IRequest<Response<FileItemsDto>>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class GetFileManagerForByIdHandler : IRequestHandler<GetFileManagerForByIdQuery, Response<FileItemsDto>>
+    {
+
+        private readonly IIdentityRepository _identityRepository;
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly IRepository<VetDocuments> _vetDocumentsRepository;
+        public GetFileManagerForByIdHandler(IIdentityRepository identityRepository, IUnitOfWork uow, IMapper mapper, IRepository<VetDocuments> vetDocumentsRepository)
+        {
+            _identityRepository = identityRepository;
+            _uow = uow;
+            _mapper = mapper;
+            _vetDocumentsRepository = vetDocumentsRepository;
+        }
+
+        public async Task<Response<FileItemsDto>> Handle(GetFileManagerForByIdQuery request, CancellationToken cancellationToken)
+        {
+
+            var response = Response<FileItemsDto>.Success(200);
+            response.Data = new FileItemsDto();
+            try
+            {
+                var _documents = await _vetDocumentsRepository.GetByIdAsync(request.Id);
+                if (_documents != null)
+                {
+                    Item _file = new Item();
+                    _file.Id = _documents.Id;
+                    _file.Name = _documents.FileName;
+                    _file.CreatedAt = _documents.CreateUsers;
+                    _file.Type = ".txt";
+                    _file.Size = "0Mb";
+                    _file.Contents = "";
+                    _file.CreatedBy = "";
+                    _file.Description = "";
+                    _file.FolderId = Guid.Empty;
+                    response.Data.Files.Add(_file);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Response<FileItemsDto>.Fail(ex.Message, 400);
+            }
+            return response;
+
+
+        }
+    }
+}

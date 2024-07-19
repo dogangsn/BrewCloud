a33219fb27449b21dc048341f11dfe738bc83837
Dogan Gunes <dogangns.98@gmail.com>
2024-07-19 03:12:10 +0300

    .

--- Services/Vet/VetSystems.Vet.Domain/VetSystems.Vet.Domain/Entities/VetLogs.cs
+++ Services/Vet/VetSystems.Vet.Domain/VetSystems.Vet.Domain/Entities/VetLogs.cs
@@ -12,12 +12,12 @@ namespace VetSystems.Vet.Domain.Entities
         public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public Guid UserId { get; set; }
-        public string UserName { get; set; }
-        public string OldValue { get; set; }
-        public string NewValue { get; set; }
-        public string FieldName { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string OldValue { get; set; } = string.Empty;
+        public string NewValue { get; set; } = string.Empty;
+        public string FieldName { get; set; } = string.Empty;
         public Guid TenantId { get; set; }
-        public string TableName { get; set; }
-        public string MasterId { get; set; }
+        public string TableName { get; set; } = string.Empty;
+        public string MasterId { get; set; } = string.Empty;
     }
 }
--- Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Persistence/VetDbContext.cs
+++ Services/Vet/VetSystems.Vet.Infrastructure/VetSystems.Vet.Infrastructure/Persistence/VetDbContext.cs
@@ -247,10 +247,10 @@ namespace VetSystems.Vet.Infrastructure.Persistence
                                 Date = logDate,
                                 UserId = _identityRepository.Account.UserId,
                                 UserName = _identityRepository.Account.UserName,
-                                OldValue = originalValue?.ToString(),
+                                OldValue = (originalValue == null ? string.Empty : originalValue?.ToString()),
                                 TableName = entity.Entity.GetType().Name,
                                 FieldName = property.Name,
-                                NewValue = updatedValue?.ToString(),
+                                NewValue =  (updatedValue == null ? string.Empty : updatedValue?.ToString()),
                                 MasterId = masterId,
                                 TenantId = _identityRepository.TenantId
                             });

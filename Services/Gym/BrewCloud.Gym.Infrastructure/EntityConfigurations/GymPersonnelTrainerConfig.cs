d29be2c55101dc8625c47cb98cde8c08e78bb27a
E-Tas93 <alitas7875@gmail.com>
2024-10-29 01:08:58 +0300

    add GymPersonnelTrainer table

---
+++ Services/Gym/BrewCloud.Gym.Infrastructure/EntityConfigurations/GymPersonnelTrainerConfig.cs
@@ -0,0 +1,15 @@
+﻿using BrewCloud.Gym.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BrewCloud.Gym.Infrastructure.EntityConfigurations
+{
+    public class GymPersonnelTrainerConfig : IEntityTypeConfiguration<GymPersonnelTrainer>
+    {
+        public void Configure(EntityTypeBuilder<GymPersonnelTrainer> entity)
+        {
+            entity.HasKey(e => e.Id)
+                   .HasName("GymPersonnelTrainer_pkey");
+        }
+    }
+}
---
+++ Services/Gym/BrewCloud.Gym.Infrastructure/Migrations/20241028220745_updateData_7.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrewCloud.Gym.Infrastructure.Migrations
+{
+    public partial class updateData_7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gympersonneltrainer",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    branchid = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    firstname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phonenumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    experiences = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gender = table.Column<int>(type: "int", nullable: false),
+                    birthdate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    identitynumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    capacity = table.Column<short>(type: "smallint", nullable: true),
+                    isstaff = table.Column<bool>(type: "bit", nullable: false),
+                    recid = table.Column<int>(type: "int", nullable: false),
+                    createdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleteddate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deletedusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    updateusers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createusers = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("GymPersonnelTrainer_pkey", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gympersonneltrainer");
+        }
+    }
+}
